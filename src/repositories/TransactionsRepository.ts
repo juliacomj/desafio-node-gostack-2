390a5519a9921a58b2257d891b515137096e4231
Júlia Roldi <julialcantara.ra@gmail.com>
2020-05-31 20:00:06 -0300

    desafio node gostack

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeBalance = this.transactions.reduce((sum, transaction) => {
+      if (transaction.type === 'income') {
+        return transaction.value + sum;
+      }
+      return sum;
+    }, 0);
+    const outcomeBalance = this.transactions.reduce((sum, transaction) => {
+      if (transaction.type === 'outcome') {
+        return transaction.value + sum;
+      }
+      return sum;
+    }, 0);
+    const totalBalance = incomeBalance - outcomeBalance;
+    const balance = {
+      income: incomeBalance,
+      outcome: outcomeBalance,
+      total: totalBalance,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
